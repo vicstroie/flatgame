3fbf92252549aab60267c29e23dcd9109e87f73f
victorstroie <vas7119@nyu.edu>
2024-02-01 16:18:35 -0500

    added preliminary sound code

---
+++ StroieVictorFlatGame/Assets/Scripts/Sound.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+
+    private bool isPlaying = false;
+    private AudioSource sound;
+
+    private void Start()
+    {
+        sound = this.gameObject.GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        
+    }
+}
